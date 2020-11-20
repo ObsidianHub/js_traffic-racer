f59a14f8adc2279706697fd32291a612fc548bba
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-11-21 00:14:46 +0200

    create set high score function

--- js/trafficRacer.js
+++ js/trafficRacer.js
@@ -165,4 +165,12 @@ $(function () {
     restart_btn.focus();
     setHighScore();
   }
+
+  function setHighScore() {
+    if (high_score < parseInt(score.text())) {
+      high_score = parseInt(score.text());
+      localStorage.setItem("high_score", parseInt(score.text()));
+    }
+    $("#high_score").text(high_score);
+  }
 });
